7494bf9e47745fcfd406698fd9b2786d7c85f2bb
Mylenna Alcantara Araujo <mylenna.a.a.corinthiana@gmail.com>
2024-03-15 14:32:52 -0300

    feat: pesquisa pelo teclado virtual e seleção por dois clicks

--- src/components/cityModal/index.jsx
+++ src/components/cityModal/index.jsx
@@ -1,12 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import Keyboard from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
+import * as FC from "../formClient/formClient";
 import { Loading } from "../loading";
 import * as CM from "./cityModal";
 
-export const CityModal = ({ onClose }) => {
+export const CityModal = ({ onClose, dadosCliente, setDadosCliente }) => {
     const [municipios, setMunicipios] = useState([]);
     const [busca, setBusca] = useState("");
-
-    // Estado para verificar se obteve 200 da api caso não, mostre a mensagem de sem dados
+    const [layout, setLayout] = useState("default");
+    const keyboard = useRef();
+    const [keyboardVisibility, setKeyboardVisibility] = useState(false);
     const [carregado, setCarregado] = useState(false);
 
     useEffect(() => {
@@ -25,15 +29,62 @@ export const CityModal = ({ onClose }) => {
         return city.nome.toLowerCase().includes(busca);
     })
 
+    const selecionar = (municipio) => {
+        setDadosCliente({
+            ...dadosCliente, 
+            cod_municipio: municipio.id,
+            CIDADE: municipio.nome,
+            estado: municipio.microrregiao.mesorregiao.UF.sigla
+        });
+        onClose();
+    }
+
+    const onChange = (input) => {
+        setBusca(input);
+    };
+
+    const handleShift = () => {
+        const newLayoutName = layout === "default" ? "shift" : "default";
+        setLayout(newLayoutName);
+    };
+
+    const onKeyPress = (button) => {
+        if (button === "{shift}" || button === "{lock}") handleShift();
+    };
+
+    const isTabletWithoutMouse = () => {
+        return (
+            /windows/i.test(navigator.userAgent) &&
+            navigator.maxTouchPoints &&
+            navigator.maxTouchPoints > 1
+        );
+    };
+
+    const handleChange = (e) => {
+        const input = e.target.value;
+        setBusca(input);
+        if(keyboard.current){
+            keyboard.current.setInput(input);
+        }
+    };
+
+    const onFocusHandler = (e) => {
+        if (keyboard.current) {
+            keyboard.current.setInput(busca);
+        }
+        
+        setKeyboardVisibility(true);
+    };
+
     return (
-        <CM.Modal>
+        <CM.Modal onClick={(e)=> e.stopPropagation()}>
             <CM.Container>
                 <CM.Header>
                     <h3>Cidades</h3>
                     <button onClick={onClose}>X</button>
                 </CM.Header>
                 <div className="div-busca">
-                    <input value={busca} onChange={(e) => setBusca(e.target.value)} placeholder="Buscar..." />
+                    <input value={busca} onChange={handleChange} onFocus={onFocusHandler} placeholder="Buscar..." />
                     <button><img alt="lupa" src="/images/LUPA.png" /></button>
                 </div>
                 <div className="div-table">
@@ -47,7 +98,7 @@ export const CityModal = ({ onClose }) => {
                             <tbody>
                                 {resultado.map((municipio) => {
                                     return (
-                                        <tr key={municipio.id} >
+                                        <tr key={municipio.id} onDoubleClick={selecionar.bind(this, municipio)}>
                                             <td>{municipio.id}</td>
                                             <td>{municipio.nome}</td>
                                             <td>{municipio.microrregiao.mesorregiao.UF.sigla}</td>
@@ -61,6 +112,17 @@ export const CityModal = ({ onClose }) => {
                     )}
                 </div>
             </CM.Container>
+            {keyboardVisibility && isTabletWithoutMouse() ? (
+                <FC.Keyboard>
+                    <Keyboard
+                        keyboardRef={(r) => (keyboard.current = r)}
+                        layoutName={layout}
+                        onChange={onChange}
+                        onKeyPress={onKeyPress}
+                        disableCaretPositioning={false}
+                    />
+                </FC.Keyboard>
+            ) : null}
         </CM.Modal>
     )
 }
