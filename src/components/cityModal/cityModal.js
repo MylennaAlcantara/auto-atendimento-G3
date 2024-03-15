ba39efb850d369d2d6b3a36fc7a4f85f84cb7095
Mylenna Alcantara Araujo <mylenna.a.a.corinthiana@gmail.com>
2024-03-15 10:39:39 -0300

    feat: pesquisa e listagem das cidades com componente de carregamento

--- src/components/cityModal/cityModal.js
+++ src/components/cityModal/cityModal.js
@@ -21,7 +21,36 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: space-between;
+    justify-content: start;
+    .div-busca{
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        input{
+            margin-top: 5px;
+            height: 30px;
+            width: 85%;
+        }
+        button{
+            height: 30px;
+            width: 30px;
+            margin: 5px;
+            img{
+                height: 100%;
+                width: 100%;
+            }
+        }
+    }
+    .div-table{
+        width: 100%;
+        height: 100%;
+        margin-top: 5px;
+        overflow: auto;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 `
 
 export const Header = styled.header`
@@ -41,5 +70,32 @@ export const Header = styled.header`
     button:hover{
         background-color: red;
         color: white;
+    }
+`
+
+export const Table = styled.table`
+    width: 95%;
+    height: 100%;
+    margin: auto;    
+    border-collapse: collapse;
+    thead{
+        position: sticky;
+        top: 0px;
+        background-color: #F0F0F0;
+    }
+    th,td{
+        white-space: nowrap;
+        border: 1px solid grey;
+    }
+    tr:hover td{
+        background-color: #87CEFA;
+        cursor: pointer;
+    }
+    
+    tr:nth-child(even){
+        background-color: #ffffe6;
+    }
+    tr:nth-child(odd){
+        background-color: #f0f0f0;   
     }
 `
--- src/components/cityModal/index.jsx
+++ src/components/cityModal/index.jsx
@@ -1,13 +1,65 @@
+import { useEffect, useState } from "react";
+import { Loading } from "../loading";
 import * as CM from "./cityModal";
 
-export const CityModal = ({onClose}) => {
-    return(
+export const CityModal = ({ onClose }) => {
+    const [municipios, setMunicipios] = useState([]);
+    const [busca, setBusca] = useState("");
+
+    // Estado para verificar se obteve 200 da api caso não, mostre a mensagem de sem dados
+    const [carregado, setCarregado] = useState(false);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch("https://servicodados.ibge.gov.br/api/v1/localidades/municipios");
+            const data = await response.json();
+            setMunicipios(data);
+            if (response.status === 200) {
+                setCarregado(true);
+            }
+        }
+        fetchData();
+    }, []);
+
+    const resultado = Array.isArray(municipios) && municipios.filter((city) => {
+        return city.nome.toLowerCase().includes(busca);
+    })
+
+    return (
         <CM.Modal>
             <CM.Container>
                 <CM.Header>
                     <h3>Cidades</h3>
                     <button onClick={onClose}>X</button>
                 </CM.Header>
+                <div className="div-busca">
+                    <input value={busca} onChange={(e) => setBusca(e.target.value)} placeholder="Buscar..." />
+                    <button><img alt="lupa" src="/images/LUPA.png" /></button>
+                </div>
+                <div className="div-table">
+                    {carregado ? (
+                        <CM.Table>
+                            <thead>
+                                <th>Código</th>
+                                <th>Nome</th>
+                                <th>UF</th>
+                            </thead>
+                            <tbody>
+                                {resultado.map((municipio) => {
+                                    return (
+                                        <tr key={municipio.id} >
+                                            <td>{municipio.id}</td>
+                                            <td>{municipio.nome}</td>
+                                            <td>{municipio.microrregiao.mesorregiao.UF.sigla}</td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </CM.Table>
+                    ) : (
+                        <Loading />
+                    )}
+                </div>
             </CM.Container>
         </CM.Modal>
     )
